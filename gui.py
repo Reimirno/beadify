164b53d1a3195f8b06b84140b59a7ff834f569ab
Reimirno <long_yu@berkeley.edu>
2023-12-29 20:07:53 -0800

    refac: rewrite frontend

--- gui.py
+++ gui.py
@@ -40,7 +40,110 @@ class ColorMapChoice:
     
     def get_complete_map(self) -> dict[str, ColorEntryMatch]:
         return {orig_hex_str: self.get_mapped_color_for(orig_hex_str) for orig_hex_str in self.color_map}
+
+class SrcImgCanvas(tk.Canvas):
+    def __init__(self, root, image_fixture: ImageFixture, **kwargs):
+        self.image_fixture = image_fixture
+        width, height = image_fixture.width, image_fixture.height
+        super().__init__(root, width=width*CELL_SIZE, height=height*CELL_SIZE, **kwargs)
+        for x in range(width):
+            for y in range(height):
+                position = x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE
+                self.create_rectangle(position, fill=image_fixture.pixels[x][y], tags=f"rect_{x}_{y}")
     
+    def update(self, var_outline):
+        width, height = self.image_fixture.width, self.image_fixture.height
+        for x in range(width):
+            for y in range(height):
+                self.itemconfig(f"rect_{x}_{y}", outline='grey' if var_outline else '')
+
+class RltImgCanvas(tk.Canvas):
+    def __init__(self, root, image_fixture: ImageFixture, **kwargs):
+        self.image_fixture = image_fixture
+        self.change_focus_lambda = lambda x, y: None
+        width, height = image_fixture.width, image_fixture.height
+        super().__init__(root, width=width*CELL_SIZE, height=height*CELL_SIZE, **kwargs)
+        for x in range(width):
+            for y in range(height):
+                position = x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE
+                cell = self.create_rectangle(position, tags=f"rect_{x}_{y}")
+                text = self.create_text((x + 0.5) * CELL_SIZE, (y + 0.5) * CELL_SIZE, text='', tags=f"text_{x}_{y}")
+                self.tag_bind(text, '<Button-1>', lambda event, x=x, y=y: self.change_focus_lambda(x, y))
+                self.tag_bind(cell, '<Button-1>', lambda event, x=x, y=y: self.change_focus_lambda(x, y))
+    
+    def assign_event(self, change_focus_lambda):
+        self.change_focus_lambda = change_focus_lambda
+
+    def update(self, var_map_choice: ColorMapChoice, var_label:  bool, var_outline: bool):
+        width, height = self.image_fixture.width, self.image_fixture.height
+        for x in range(width):
+            for y in range(height):
+                chosen_mapped_color = var_map_choice.get_mapped_color_for(self.image_fixture.pixels[x][y])
+                self.itemconfig(f"rect_{x}_{y}", fill='#' + chosen_mapped_color.color.hex,
+                                    outline='grey' if var_outline else '')
+                self.itemconfig(f"text_{x}_{y}", text=chosen_mapped_color.color.coco if var_label else '',
+                                    fill=get_contrasting_text_color_hex_str(chosen_mapped_color.color.hex))
+
+class FocusPalette(tk.Canvas):
+    def __init__(self, root, image_fixture: ImageFixture, **kwargs):
+        self.image_fixture = image_fixture
+        self.change_map_choice_lambda = lambda x: None
+        big_cell_size = 200
+        medium_cell_size = 100
+        spacing_big = 50
+        spacing_medium = 5
+        canvas_width = max(2 * big_cell_size + spacing_big, K * medium_cell_size + (K - 1) * spacing_medium)
+        canvas_height = big_cell_size + medium_cell_size + spacing_medium
+        super().__init__(root, width=canvas_width, height=canvas_height, **kwargs)
+
+        x = (canvas_width - 2 * big_cell_size - spacing_big) / 2
+        y = 0
+        self.create_rectangle(x, y, x + big_cell_size, y + big_cell_size, tags='target_color')
+        self.create_text(x + big_cell_size / 2, y + big_cell_size / 2, text='', tags='target_text')
+
+        x += big_cell_size + spacing_big
+        self.create_rectangle(x, y, x + big_cell_size, y + big_cell_size, tags='current_color')
+        self.create_text(x + big_cell_size / 2, y + big_cell_size / 2, text='', tags='current_text')
+
+        y = big_cell_size + spacing_medium
+        for i in range(K):
+            x = i * (medium_cell_size + spacing_medium)
+            x += (canvas_width - K * medium_cell_size - (K - 1) * spacing_medium) / 2
+            cell = self.create_rectangle(x, y, x + medium_cell_size, y + medium_cell_size, tags=f'option_color_{i}')
+            text = self.create_text(x + medium_cell_size / 2, y + medium_cell_size / 2, text='', tags=f'option_text_{i}')
+            self.tag_bind(text, '<Button-1>', lambda event, i=i: self.change_map_choice_lambda(i))
+            self.tag_bind(cell, '<Button-1>', lambda event, i=i: self.change_map_choice_lambda(i))
+    
+    def assign_event(self, change_map_choice_lambda):
+        self.change_map_choice_lambda = change_map_choice_lambda
+
+    def update(self, var_map_choice: ColorMapChoice, var_fx: int, var_fy: int):
+        orig_hex_str = self.image_fixture.pixels[var_fx][var_fy]
+        chosen_mapped_color = var_map_choice.get_mapped_color_for(orig_hex_str)
+        all_mapped_colors = var_map_choice.get_all_mapped_colors_for(orig_hex_str)
+
+        self.itemconfig('target_color', fill=orig_hex_str)
+        self.itemconfig('target_text', text="TARGET-" + orig_hex_str,
+                        fill=get_contrasting_text_color_hex_str(orig_hex_str))
+
+        self.itemconfig('current_color', fill='#' + chosen_mapped_color.color.hex)
+        self.itemconfig('current_text', text="CURRENT-" + chosen_mapped_color.color.coco,
+                        fill=get_contrasting_text_color_hex_str(chosen_mapped_color.color.hex))
+
+        for i in range(K):
+            cur_option_color = all_mapped_colors[i]
+            self.itemconfig(f'option_color_{i}', fill='#' + cur_option_color.color.hex)
+            self.itemconfig(f'option_text_{i}', text=cur_option_color.color.coco,
+                            fill=get_contrasting_text_color_hex_str(cur_option_color.color.hex))
+
+class ButtonGroup(tk.Frame):
+    def __init__(self, root, **kwargs):
+        super().__init__(root, **kwargs)
+    
+    def add_button(self, text, command):
+        button = tk.Button(self, text=text, command=command)
+        button.pack(side='left')
+
 def load_src_img(repository: list[ColorEntry], src_img_path) -> ImageFixture:
     image = Image.open(src_img_path)
     pixels = image.load()
@@ -91,213 +194,60 @@ def main():
     image_fixture = load_src_img(repository, cur_img_path)
 
     var_map_choice = ColorMapChoice(image_fixture)
-    change_map_choice_lambda = lambda new_val: None
     var_fx, var_fy = 0, 0
-    change_focus_lambda = lambda x, y: None
     var_label, var_outline = True, True
-    change_style_lambda = lambda: None
 
-    def draw_src_img(var_outline):
-        width, height = image_fixture.width, image_fixture.height
-        canvas = tk.Canvas(root, width=width*CELL_SIZE, height=height*CELL_SIZE)
-        for x in range(width):
-            for y in range(height):
-                position = x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE
-                canvas.create_rectangle(position, fill=image_fixture.pixels[x][y], outline='grey' if var_outline else '',
-                                        tags=f"rect_{x}_{y}")
-        return canvas
-
-    def update_src_img(canvas, new_var_outline):
-        width, height = image_fixture.width, image_fixture.height
-        for x in range(width):
-            for y in range(height):
-                canvas.itemconfig(f"rect_{x}_{y}", outline='grey' if new_var_outline else '')
-
-    src_canvas = draw_src_img(var_outline)
+    src_canvas = SrcImgCanvas(root, image_fixture=image_fixture)
+    src_canvas.update(var_outline)
     src_canvas.grid(row=0, column=0)
 
-    def draw_rlt_img(map_choice: ColorMapChoice, var_label: bool, var_outline: bool):
-        width, height = image_fixture.width, image_fixture.height
-        canvas = tk.Canvas(root, width=width*CELL_SIZE, height=height*CELL_SIZE)
-        for x in range(width):
-            for y in range(height):
-                chosen_mapped_color = map_choice.get_mapped_color_for(image_fixture.pixels[x][y])
-                position = x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE
-                cell = canvas.create_rectangle(position, fill='#' + chosen_mapped_color.color.hex, 
-                                               outline='grey' if var_outline else '', tags=f"rect_{x}_{y}")
-                center_x, center_y = (x + 0.5) * CELL_SIZE, (y + 0.5) * CELL_SIZE
-                text = canvas.create_text(center_x, center_y, text=chosen_mapped_color.color.coco if var_label else '', 
-                                          fill=get_contrasting_text_color_hex_str(chosen_mapped_color.color.hex),
-                                          tags=f"text_{x}_{y}")
-                nonlocal change_focus_lambda
-                canvas.tag_bind(text, '<Button-1>', lambda event, x=x, y=y: change_focus_lambda(x, y))
-                canvas.tag_bind(cell, '<Button-1>', lambda event, x=x, y=y: change_focus_lambda(x, y))
-        return canvas
-
-    def update_rlt_img(canvas, new_map_choice: ColorMapChoice, new_var_label, new_var_outline):
-        width, height = image_fixture.width, image_fixture.height
-        for x in range(width):
-            for y in range(height):
-                chosen_mapped_color = new_map_choice.get_mapped_color_for(image_fixture.pixels[x][y])
-                canvas.itemconfig(f"rect_{x}_{y}", fill='#' + chosen_mapped_color.color.hex,
-                                  outline='grey' if new_var_outline else '')
-                canvas.itemconfig(f"text_{x}_{y}", text=chosen_mapped_color.color.coco if new_var_label else '',
-                                  fill=get_contrasting_text_color_hex_str(chosen_mapped_color.color.hex))
-
-    rlt_canvas = draw_rlt_img(var_map_choice, var_label, var_outline)
+    rlt_canvas = RltImgCanvas(root, image_fixture=image_fixture)
+    rlt_canvas.update(var_map_choice, var_label, var_outline)
     rlt_canvas.grid(row=0, column=1)
 
-    def draw_focus_palette(map_choice: ColorMapChoice, fx: int, fy: int):
-        big_cell_size = 200
-        medium_cell_size = 100
-        spacing_big = 50
-        spacing_medium = 5
-        canvas_width = max(2 * big_cell_size + spacing_big, K * medium_cell_size + (K - 1) * spacing_medium)
-        canvas_height = big_cell_size + medium_cell_size + spacing_medium
-
-        orig_hex_str = image_fixture.pixels[fx][fy]
-        chosen_mapped_color = map_choice.get_mapped_color_for(orig_hex_str)
-        all_mapped_colors = map_choice.get_all_mapped_colors_for(orig_hex_str)
-
-        canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
-
-        x = (canvas_width - 2 * big_cell_size - spacing_big) / 2
-        y = 0
-        canvas.create_rectangle(x, y, x + big_cell_size, y + big_cell_size, fill=orig_hex_str, tags='target_color')
-        canvas.create_text(x + big_cell_size / 2, y + big_cell_size / 2, text="TARGET-" + orig_hex_str, 
-                            fill=get_contrasting_text_color_hex_str(orig_hex_str), tags='target_text')
-
-        x += big_cell_size + spacing_big
-        canvas.create_rectangle(x, y, x + big_cell_size, y + big_cell_size, 
-                                fill='#' + chosen_mapped_color.color.hex, tags='current_color')
-        canvas.create_text(x + big_cell_size / 2, y + big_cell_size / 2, 
-                           text="CURRENT-" + chosen_mapped_color.color.coco, 
-                           fill=get_contrasting_text_color_hex_str(chosen_mapped_color.color.hex), 
-                           tags='current_text')
-
-        y = big_cell_size + spacing_medium
-        for i in range(K):
-            cur_option_color = all_mapped_colors[i]
-            x = i * (medium_cell_size + spacing_medium)
-            x += (canvas_width - K * medium_cell_size - (K - 1) * spacing_medium) / 2
-            cell = canvas.create_rectangle(x, y, x + medium_cell_size, y + medium_cell_size, 
-                                           fill='#' + cur_option_color.color.hex, tags=f'option_color_{i}')
-            text = canvas.create_text(x + medium_cell_size / 2, y + medium_cell_size / 2, 
-                                      text=cur_option_color.color.coco, 
-                                      fill=get_contrasting_text_color_hex_str(cur_option_color.color.hex), 
-                                      tags=f'option_text_{i}')
-            nonlocal change_map_choice_lambda
-            canvas.tag_bind(text, '<Button-1>', lambda event, i=i: change_map_choice_lambda(i))
-            canvas.tag_bind(cell, '<Button-1>', lambda event, i=i: change_map_choice_lambda(i))
-
-        return canvas
-
-    def update_focus_palette(canvas, new_map_choice: ColorMapChoice, new_fx: int, new_fy: int):
-        orig_hex_str = image_fixture.pixels[new_fx][new_fy]
-        chosen_mapped_color = new_map_choice.get_mapped_color_for(orig_hex_str)
-        all_mapped_colors = new_map_choice.get_all_mapped_colors_for(orig_hex_str)
-
-        # Update target color and text
-        canvas.itemconfig('target_color', fill=orig_hex_str)
-        canvas.itemconfig('target_text', text="TARGET-" + orig_hex_str,
-                          fill=get_contrasting_text_color_hex_str(orig_hex_str))
-
-        # Update current color and text
-        canvas.itemconfig('current_color', fill='#' + chosen_mapped_color.color.hex)
-        canvas.itemconfig('current_text', text="CURRENT-" + chosen_mapped_color.color.coco,
-                          fill=get_contrasting_text_color_hex_str(chosen_mapped_color.color.hex))
-
-        # Update option colors and texts
-        for i in range(K):
-            cur_option_color = all_mapped_colors[i]
-            canvas.itemconfig(f'option_color_{i}', fill='#' + cur_option_color.color.hex)
-            canvas.itemconfig(f'option_text_{i}', text=cur_option_color.color.coco,
-                              fill=get_contrasting_text_color_hex_str(cur_option_color.color.hex))
-            
-    focus_canvas = draw_focus_palette(var_map_choice, var_fx, var_fy)
-    focus_canvas.grid(row=1, column=0, columnspan=2)
-
-    def draw_color_collection(map_choice: ColorMapChoice):
-        color_size = 40
-        color_interval = 5
-        width = min(800, map_choice.count * (color_size + color_interval) - color_interval)
-        height = color_size
-        canvas = tk.Canvas(root, width=width, height=height)
-        x, y = 0, 0
-        complete_map = map_choice.get_complete_map()
-        for i, hex_str in enumerate(complete_map):
-            chosen_mapped_color = complete_map[hex_str]
-            canvas.create_rectangle(x, y, x + color_size, y + color_size, 
-                                    fill='#' + chosen_mapped_color.color.hex,
-                                    outline='grey', tags=f"color_{i}")
-            canvas.create_text(x + color_size / 2, y + color_size / 2, 
-                               text=chosen_mapped_color.color.coco,
-                                fill=get_contrasting_text_color_hex_str(chosen_mapped_color.color.hex),
-                                tags=f"text_{i}")
-            x += color_size
-            if x >= width:
-                x = 0
-                y += CELL_SIZE
-        return canvas
-    
-    def update_color_collection(canvas, new_map_choice: ColorMapChoice):
-        complete_map = new_map_choice.get_complete_map()
-        for i, hex_str in enumerate(complete_map):
-            chosen_mapped_color = complete_map[hex_str]
-            canvas.itemconfig(f"color_{i}", fill='#' + chosen_mapped_color.color.hex)
-            canvas.itemconfig(f"text_{i}", text=chosen_mapped_color.color.coco,
-                                fill=get_contrasting_text_color_hex_str(chosen_mapped_color.color.hex))
-        
-
-    color_collection_canvas = draw_color_collection(var_map_choice)
-    color_collection_canvas.grid(row=2, column=0, columnspan=2)
+    focus_palette = FocusPalette(root, image_fixture=image_fixture)
+    focus_palette.update(var_map_choice, var_fx, var_fy)
+    focus_palette.grid(row=1, column=0, columnspan=2)
 
     def change_focus_lambda(x, y):
         nonlocal var_fx, var_fy
         var_fx, var_fy = x, y
-        update_focus_palette(focus_canvas, var_map_choice, var_fx, var_fy)
+        focus_palette.update(var_map_choice, var_fx, var_fy)
+    rlt_canvas.assign_event(change_focus_lambda)
 
     def change_map_choice_lambda(new_val):
         nonlocal var_map_choice, var_fx, var_fy, var_label, var_outline
-        orig_hex_str = image_fixture[var_fx][var_fy]
+        orig_hex_str = image_fixture.pixels[var_fx][var_fy]
         var_map_choice.set_mapped_color_for(orig_hex_str, new_val)
-        update_rlt_img(rlt_canvas, var_map_choice, var_label, var_outline)
-        update_color_collection(color_collection_canvas, var_map_choice)
-        update_focus_palette(focus_canvas, var_map_choice, var_fx, var_fy)
+        rlt_canvas.update(var_map_choice, var_label, var_outline)
+        focus_palette.update(var_map_choice, var_fx, var_fy)
+    focus_palette.assign_event(change_map_choice_lambda)
 
     def reset_all_map_choice():
         nonlocal var_map_choice, var_fx, var_fy, var_label, var_outline
         var_map_choice.reset_all_map_choice()
-        update_rlt_img(rlt_canvas, var_map_choice, var_label, var_outline)
-        update_color_collection(color_collection_canvas, var_map_choice)
-        update_focus_palette(focus_canvas, var_map_choice, var_fx, var_fy)
+        rlt_canvas.update(var_map_choice, var_label, var_outline)
+        focus_palette.update(var_map_choice, var_fx, var_fy)
     
-    def change_style_lambda():
+    def change_label_lambda():
         nonlocal var_label, var_outline
         var_label = not var_label
-        var_outline = not var_outline
-        update_src_img(src_canvas, var_outline)
-        update_rlt_img(rlt_canvas, var_map_choice, var_label, var_outline)
-
-    def draw_button_groups():
-        button_frame = tk.Frame(root)
-
-        style_button = tk.Button(button_frame, text="Style", command=lambda: change_style_lambda())
-        style_button.grid(row=0, column=0)
-
-        reset_button = tk.Button(button_frame, text="Reset", command=reset_all_map_choice)
-        reset_button.grid(row=0, column=1)
-
-        save_button = tk.Button(button_frame, text="Save", command=lambda: export_canvas(rlt_canvas))
-        save_button.grid(row=0, column=2)
-
-        exit_button = tk.Button(button_frame, text="Exit", command=root.destroy)
-        exit_button.grid(row=0, column=3)
-
-        return button_frame
+        src_canvas.update(var_outline)
+        rlt_canvas.update(var_map_choice, var_label, var_outline)
     
-    button_frame = draw_button_groups()
-    button_frame.grid(row=3, column=0, columnspan=2)
+    def change_outline_lambda():
+        nonlocal var_label, var_outline
+        var_outline = not var_outline
+        src_canvas.update(var_outline)
+        rlt_canvas.update(var_map_choice, var_label, var_outline)
+
+    btn_group = ButtonGroup(root)
+    btn_group.add_button("Label", change_label_lambda)
+    btn_group.add_button("Outline", change_outline_lambda)
+    btn_group.add_button("Reset", reset_all_map_choice)
+    btn_group.add_button("Save", lambda: export_canvas(rlt_canvas))
+    btn_group.add_button("Exit", root.destroy)
+    btn_group.grid(row=3, column=0, columnspan=2)
 
     root.mainloop()
 
